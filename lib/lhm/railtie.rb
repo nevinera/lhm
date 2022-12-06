28ee94f3984126346dd8a3e51a098d97bf6b58eb
Eric Mueller <nevinera@gmail.com>
2022-12-06 15:03:21 -0500

    update the railtie to check Lhm.inline_allowed? before inlining migrations

--- lib/lhm/railtie.rb
+++ lib/lhm/railtie.rb
@@ -2,7 +2,7 @@ module Lhm
   class Railtie < Rails::Railtie
     initializer "lhm.test_setup" do
       if Rails.env.test? || Rails.env.development?
-        Lhm.execute_inline!
+        Lhm.execute_inline! if Lhm.inline_allowed?
       end
     end
   end
